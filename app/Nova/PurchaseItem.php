98cf310bc828f16520ec4ac57d60b1ccd1593469
Fathur Rohman <hi.fathur.rohman@gmail.com>
2021-07-21 20:24:18 +0700

    update fields

--- app/Nova/PurchaseItem.php
+++ app/Nova/PurchaseItem.php
@@ -24,7 +24,7 @@ class PurchaseItem extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'code';
 
     /**
      * The columns that should be searched.
