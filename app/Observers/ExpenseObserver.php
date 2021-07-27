d0ac3f80af447cc78b20a37015740b4d17431e92
Fathur Rohman <hi.fathur.rohman@gmail.com>
2021-07-27 15:40:31 +0700

    Feature/22 expensable (#30)
    * Feature/expenses (#16)
    
    * feat(expense): create expense
    
    * feat(expense): adjust account instead of bank
    
    * Hotfix/6 company (#19)
    
    * Feature/expenses (#17)
    
    * feat(expense): create expense
    
    * feat(expense): adjust account instead of bank
    
    * Feature/expenses (#16) (#18)
    
    * feat(expense): create expense
    
    * feat(expense): adjust account instead of bank
    
    * fix(issue#6): fix issue #6, only view specific company
    
    * feat(sales): create sales feature (#23)
    
    * feat(expenses): add expansable transaction
    
    * fix(expense): bug fix total expense for transaction

--- app/Observers/ExpenseObserver.php
+++ app/Observers/ExpenseObserver.php
@@ -2,23 +2,77 @@
 
 namespace App\Observers;
 
+use App\Models\Company;
 use App\Models\Expense;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class ExpenseObserver
 {
+    /**
+     * @param Expense $expense
+     */
     public function creating(Expense $expense)
     {
+        $company = $this->decideCompany($expense);
 
-//        $company = $expense->company;
-        $company = Auth::user()->companies()->first();
+        $this->generateCode($company, $expense);
+    }
 
-        $expense->company_id = $company->id;
+    public function created(Expense $expense)
+    {
+        $expense->invoice()->create(
+            [
+                'balance_due' => $expense->total
+            ]
+        );
 
+        $this->adjustTotal($expense);
+    }
 
+    public function updating(Expense $expense)
+    {
+    }
+
+    public function updated(Expense $expense)
+    {
+        $expenseTotal = $expense->total;
+        $invoicePaid = $expense->invoice->paid;
+        $invoiceBalanceDue = $expenseTotal - $invoicePaid;
+        $expense->invoice()->update(
+            [
+                'balance_due' => $invoiceBalanceDue
+            ]
+        );
+
+        //
+        $this->adjustTotal($expense);
+    }
+
+    /**
+     * @param Expense $expense
+     * @return mixed
+     */
+    public function decideCompany(Expense $expense): Company
+    {
+        if (is_null($expense->company_id)) {
+            $company = Auth::user()->companies()->first();
+
+            $expense->company_id = $company->id;
+
+            return $company;
+        }
+
+        return $expense->company;
+    }
+
+    /**
+     * @param Company $company
+     * @param Expense $expense
+     */
+    protected function generateCode(Company $company, Expense $expense): void
+    {
         $latestCode = $company->expenses()->max('code');
-
 
         if ($latestCode) {
             $exploded = Str::of($latestCode)->explode('EXP/');
@@ -32,24 +86,33 @@ class ExpenseObserver
         }
     }
 
-    public function created(Expense $expense)
+    /**
+     * @param Expense $expense
+     */
+    public function adjustTotal(Expense $expense): void
     {
-        $expense->invoice()->create(
-            [
-            'balance_due' => $expense->total
-            ]
-        );
-    }
+        // Jika ada expansable maka update total di expansable-nya
+        // Dalam kasus ini transaction
+        if (!is_null($expense->expensable)) {
+            $expensable = $expense->expensable;
 
-    public function updated(Expense $expense)
-    {
-        $expenseTotal = $expense->total;
-        $invoicePaid = $expense->invoice->paid;
-        $invoiceBalanceDue = $expenseTotal - $invoicePaid;
-        $expense->invoice()->update(
-            [
-            'balance_due' => $invoiceBalanceDue
-            ]
-        );
+            $totalWithholding = $expensable->withholdings()->sum('nominal');
+            $totalExpense = $expensable->expenses()->sum('total');
+            $expensable->total = $expensable->sub_total - $totalWithholding + $totalExpense;
+            $expensable->save();
+        }
     }
 }
+
+// Bagaimana cara menanggulangi stress?
+// 3 Dari gejala stress di slide saya alami, apakah saya stress, tetapi saya tidak merasa stress?
+// mau datang ke psikolog harus bayar tambah stress.
+
+/**
+ * Four A to manage stress
+ *
+ * 1. Avoid
+ * 2. Alter
+ * 3. Accept
+ * 4. Adapt
+ */
